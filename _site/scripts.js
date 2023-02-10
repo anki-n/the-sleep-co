f30ae8fbe27ba2f8776627fe7a037a7a621a6920
anki-n <95865095+anki-n@users.noreply.github.com>
2023-02-10 10:23:57 +0100

    added button popup gallery

--- _site/scripts.js
+++ _site/scripts.js
@@ -21,3 +21,8 @@ lightGallery(document.getElementById('lightgallery'), {
     speed: 500,
     download: false
 });
+
+lightGallery(document.getElementById('lightgallery-2'), {
+    speed: 500,
+    download: false
+});
